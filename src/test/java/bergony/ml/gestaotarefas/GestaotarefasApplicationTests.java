484ed07c066ec09a880f8d5c34ca3b472441bc5b
Bergony <Bergony@hotmail.com>
2021-07-24 14:28:11 -0300

    Initial commit

---
+++ src/test/java/bergony/ml/gestaotarefas/GestaotarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package bergony.ml.gestaotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaotarefasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
